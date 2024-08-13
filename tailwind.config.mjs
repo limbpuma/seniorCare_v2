4a4652b8c32ba1eadda9c9f6715d4a51c1091803
Limber <limbpuma_de@hotmail.com>
2024-08-14 00:41:06 +0200

    Add new components and update Tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,29 +4,73 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: {
-          light: "#BFD2D7",
-          dark: "#004A61",
-        },
-        accent: "#FFDAA3",
+        black: '#161616', 
+        white: '#ffffff',
+        "light-gray": '#bababa', 
+        "gray-blue": '#bfd2d7', 
+        "soft-blue": '#5a8bba', 
+        "deep-blue": '#084487', 
+        "soft-beige": '#ffedcc', 
+        "soft-orange": '#F1C097',
+        primary: '#084487', 
+        accent: '#FFDAA3',
+      },
+      boxShadow: {
+        'soft': '0 4px 6px rgba(0, 0, 0, 0.1)',
+        'medium': '0 5px 15px rgba(0, 0, 0, 0.15)',
+        'hard': '0 10px 20px rgba(0, 0, 0, 0.2)'
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem'
+      },
+      borderRadius: {
+        'xl': '1rem'
+      },
+      transitionProperty: {
+        'colors': 'background-color, border-color, color, fill, stroke',
+        'opacity': 'opacity'
+      },
+      opacity: {
+        '85': '0.85',
+        '80': '0.80',
       },
     },
     fontFamily: {
-      sans: [
-        "Montserrat",
-        "system-ui",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        "Segoe UI",
-        "Roboto",
-        "Oxygen",
-        "Ubuntu",
-        "Cantarell",
-        "Open Sans",
-        "Helvetica Neue",
-        "sans-serif",
-      ],
+      'ag-h1': ['Montserrat', 'sans-serif'],
+      'ag-h2': ['Montserrat', 'sans-serif'],
+      'ag-h3': ['Montserrat', 'sans-serif'],
+      'ag-h4': ['Montserrat', 'sans-serif'],
+      'ag-h5': ['Montserrat', 'sans-serif'],
+      'ag-h6': ['Montserrat', 'sans-serif'],
+      'ag-sub-heading': ['Montserrat', 'sans-serif'],
+      'ag-button-text': ['Montserrat', 'sans-serif'],
+      'ag-body-text': ['Montserrat', 'sans-serif'],
+      'ag-testimonial-text': ['Montserrat', 'sans-serif'],
+      'ag-list-text': ['Montserrat', 'sans-serif'],
+      'ag-404-text': ['Montserrat', 'sans-serif'],
+    },
+    fontSize: {
+      'ag-h1': ['128px', { fontWeight: '700', lineHeight: 'auto' }], // Bold
+      'ag-h2': ['64px', { fontWeight: '700', lineHeight: '74px' }], // Bold
+      'ag-h3': ['48px', { fontWeight: '700', lineHeight: '58px' }], // Bold
+      'ag-h4': ['36px', { fontWeight: '700', lineHeight: '46px' }], // Bold
+      'ag-h5': ['24px', { fontWeight: '700', lineHeight: '34px' }], // Bold
+      'ag-h6': ['28px', { fontWeight: '700', lineHeight: '28px' }], // Bold
+      'ag-sub-heading': ['18px', { fontWeight: '500', lineHeight: 'auto' }], // Medium
+      'ag-button-text': ['16px', { fontWeight: '600', lineHeight: 'auto' }], // Semibold
+      'ag-body-text': ['16px', { fontWeight: '500', lineHeight: '26px' }], // Medium
+      'ag-testimonial-text': ['21px', { fontWeight: '600', fontStyle: 'italic', lineHeight: '31px' }], // Semibold Italic
+      'ag-list-text': ['16px', { fontWeight: '500', lineHeight: '46px' }], // Medium
+      'ag-404-text': ['269px', { fontWeight: '700', lineHeight: 'auto' }], // Bold
     },
   },
-  plugins: [],
+    
+  
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('flowbite/plugin'),
+  ],
 };
+
+
