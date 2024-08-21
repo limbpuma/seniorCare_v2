de9b201ffb6267131659c6513dcda5aa51a3c81d
Limber <limbpuma_de@hotmail.com>
2024-08-21 23:37:53 +0200

    Added ImageCarousel component

--- src/components/common/Slider.jsx
+++ src/components/common/Slider.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, FreeMode } from 'swiper/modules';
 
-
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
-const ReactSwiperSlider = ({ slides }) => {
+const ReactSwiperSlider = ({ slides, isImageSlider = false }) => {
   const settings = {
     modules: [Autoplay, FreeMode],
-    spaceBetween: 50,
+    spaceBetween: 30,
     slidesPerView: 'auto',
     freeMode: true,
     loop: true,
-    speed: 5000, 
+    speed: 5000,
     autoplay: {
       delay: 0,
       disableOnInteraction: false,
@@ -23,11 +22,23 @@ const ReactSwiperSlider = ({ slides }) => {
 
   return (
     <Swiper {...settings}>
-      {slides.map((slide, index) => (
+      {slides.map((slide, index, imageClasses) => (
         <SwiperSlide key={index} style={{ width: 'auto' }}>
           <div className="flex items-center justify-center text-center p-4">
-            <i className={`${slide.icon} text-ag-h2 lg:text-ag-h2 text-deep-blue mr-12`}></i>
-            <p className="ml-0 text-ag-h2 lg:text-ag-h2 font-bold text-deep-blue">{slide.text}</p>
+            {slide.customContent ? (
+              slide.customContent
+            ) : (
+              <>
+                {isImageSlider ? (
+                  <img src={slide.src} alt={slide.alt} className={imageClasses} />
+                ) : (
+                  <>
+                    <i className={`${slide.icon} text-ag-h2 lg:text-ag-h2 text-deep-blue mr-12`}></i>
+                    <p className="ml-0 text-ag-h2 lg:text-ag-h2 font-bold text-deep-blue">{slide.text}</p>
+                  </>
+                )}
+              </>
+            )}
           </div>
         </SwiperSlide>
       ))}
