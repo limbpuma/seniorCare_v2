cadb73bba1c1ecdbf8f6c845e12b38b64f968355
Limber <limbpuma_de@hotmail.com>
2024-08-25 22:44:16 +0200

    Fixed Carousel styles, updated Slider.jsx, added Banner section

--- src/components/common/Slider.jsx
+++ src/components/common/Slider.jsx
@@ -1,44 +1,65 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, FreeMode } from 'swiper/modules';
+import { Autoplay, FreeMode, Navigation } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
 
-const ReactSwiperSlider = ({ slides, isImageSlider = false, class:classes }) => {
-  const settings = {
-    modules: [Autoplay, FreeMode],
-    spaceBetween: 30,
-    slidesPerView: 'auto',
-    freeMode: true,
-    loop: true,
-    speed: 5000,
-    autoplay: {
-      delay: 0,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: false,
-    },
+const bannerSliderSettings = {
+  modules: [Autoplay, FreeMode],
+  spaceBetween: 30,
+  slidesPerView: 'auto',
+  freeMode: true,
+  loop: true,
+  speed: 3000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: false,
+  },
+};
+
+const imageCarouselSettings = {
+  modules: [Autoplay, FreeMode, Navigation],
+  spaceBetween: 40,
+  slidesPerView: 'auto',
+  freeMode: true,
+  loop: false,
+  speed: 3000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+};
+
+const ReactSwiperSlider = ({ slides, isImageSlider = false, divType = 'flex-center', imgClass = '', iconClass = '', textClass = '', titleClass = '', wrapperClass = '' }) => {
+  const settings = isImageSlider ? imageCarouselSettings : bannerSliderSettings;
+
+  const divTypes = {
+    'flex-center': 'flex items-center justify-center text-center p-4 gap-4',
+    'flex-col': 'flex flex-col text-center p-4',
+    'flex-row': 'flex items-center justify-start text-left p-4',
   };
+
+  const selectedDivClass = divTypes[divType] || divTypes['flex-center'];
 
   return (
     <Swiper {...settings}>
-      {slides.map((slide, index, imageClasses) => (
+      {slides.map((slide, index) => (
         <SwiperSlide key={index} style={{ width: 'auto' }}>
-          <div className="flex items-center justify-center text-center p-4">
-            {slide.customContent ? (
-              slide.customContent
-            ) : (
-              <>
-                {isImageSlider ? (
-                  <img src={slide.src} alt={slide.alt} />
-                ) : (
-                  <>
-                    <i className={`${slide.icon} text-ag-h2 lg:text-ag-h2 text-deep-blue mr-12`}></i>
-                    <p className="ml-0 text-ag-h2 lg:text-ag-h2 font-bold text-deep-blue">{slide.text}</p>
-                  </>
-                )}
-              </>
-            )}
+          <div className="relative">
+            {slide.src && <img src={slide.src} className={`${imgClass} w-full h-[400px] object-cover`} alt={slide.alt || 'slide image'} />}
+            <div className={wrapperClass}>
+              {slide.icon && <i className={`${slide.icon} ${iconClass}`}></i>}
+              {slide.title && <h3 className={titleClass}>{slide.title}</h3>}
+              {slide.text && <p className={textClass}>{slide.text}</p>}
+            </div>
           </div>
         </SwiperSlide>
       ))}
