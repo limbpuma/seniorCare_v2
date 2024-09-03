1c81f910767e0fdde0e48b669d2fcd427b11acae
Nombiembre <emanuelt.marin9@gmail.com>
2024-09-03 02:39:40 -0500

    added prettier, dev2 components & some sections

--- src/components/common/Slider.jsx
+++ src/components/common/Slider.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, FreeMode, Navigation } from 'swiper/modules';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, FreeMode, Navigation } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
 
 const bannerSliderSettings = {
   modules: [Autoplay, FreeMode],
   spaceBetween: 30,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   freeMode: true,
   loop: true,
   speed: 3000,
@@ -23,7 +23,7 @@ const bannerSliderSettings = {
 const imageCarouselSettings = {
   modules: [Autoplay, FreeMode, Navigation],
   spaceBetween: 40,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   freeMode: true,
   loop: false,
   speed: 3000,
@@ -33,29 +33,43 @@ const imageCarouselSettings = {
     pauseOnMouseEnter: false,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 };
 
-
-const ReactSwiperSlider = ({ slides, isImageSlider = false, divType = 'flex-center', imgClass = '', iconClass = '', textClass = '', titleClass = '', wrapperClass = '' }) => {
+const ReactSwiperSlider = ({
+  slides,
+  isImageSlider = false,
+  divType = "flex-center",
+  imgClass = "",
+  iconClass = "",
+  textClass = "",
+  titleClass = "",
+  wrapperClass = "",
+}) => {
   const settings = isImageSlider ? imageCarouselSettings : bannerSliderSettings;
 
   const divTypes = {
-    'flex-center': 'flex items-center justify-center text-center p-4 gap-4',
-    'flex-col': 'flex flex-col text-center p-4',
-    'flex-row': 'flex items-center justify-start text-left p-4',
+    "flex-center": "flex items-center justify-center text-center p-4 gap-4",
+    "flex-col": "flex flex-col text-center p-4",
+    "flex-row": "flex items-center justify-start text-left p-4",
   };
 
-  const selectedDivClass = divTypes[divType] || divTypes['flex-center'];
+  const selectedDivClass = divTypes[divType] || divTypes["flex-center"];
 
   return (
     <Swiper {...settings}>
       {slides.map((slide, index) => (
-        <SwiperSlide key={index} style={{ width: 'auto' }}>
+        <SwiperSlide key={index} style={{ width: "auto" }}>
           <div className="relative">
-            {slide.src && <img src={slide.src} className={`${imgClass} w-full h-[400px] object-cover`} alt={slide.alt || 'slide image'} />}
+            {slide.src && (
+              <img
+                src={slide.src}
+                className={`${imgClass} w-full h-[400px] object-cover`}
+                alt={slide.alt || "slide image"}
+              />
+            )}
             <div className={wrapperClass}>
               {slide.icon && <i className={`${slide.icon} ${iconClass}`}></i>}
               {slide.title && <h3 className={titleClass}>{slide.title}</h3>}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,76 +4,74 @@ export default {
   theme: {
     extend: {
       colors: {
-        black: '#161616', 
-        white: '#ffffff',
-        "light-gray": '#bababa', 
-        "gray-blue": '#bfd2d7', 
-        "soft-blue": '#5a8bba', 
-        "deep-blue": '#084487', 
-        "soft-orange": '#F2B98A',
-        "soft-beige": '#ffedcc', 
-        
-        primary: '#084487', 
-        accent: '#FFDAA3',
+        black: "#161616",
+        white: "#ffffff",
+        "light-gray": "#bababa",
+        "gray-blue": "#bfd2d7",
+        "soft-blue": "#5a8bba",
+        "deep-blue": "#084487",
+        "soft-orange": "#F2B98A",
+        "soft-beige": "#ffedcc",
+
+        primary: "#084487",
+        "primary-dark": "#004A61",
+        accent: "#FFDAA3",
       },
       boxShadow: {
-        'soft': '0 4px 6px rgba(0, 0, 0, 0.1)',
-        'medium': '0 5px 15px rgba(0, 0, 0, 0.15)',
-        'hard': '0 10px 20px rgba(0, 0, 0, 0.2)',
-        'custom-light': '0 4px 6px rgba(0, 0, 0, 0.1)',
-        'custom-heavy': '0 8px 10px rgba(0, 0, 0, 0.15)',
+        soft: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        medium: "0 5px 15px rgba(0, 0, 0, 0.15)",
+        hard: "0 10px 20px rgba(0, 0, 0, 0.2)",
+        "custom-light": "0 4px 6px rgba(0, 0, 0, 0.1)",
+        "custom-heavy": "0 8px 10px rgba(0, 0, 0, 0.15)",
       },
       spacing: {
-        '128': '32rem',
-        '144': '36rem'
+        128: "32rem",
+        144: "36rem",
       },
       borderRadius: {
-        'xl': '1rem'
+        xl: "1rem",
       },
       transitionProperty: {
-        'colors': 'background-color, border-color, color, fill, stroke',
-        'opacity': 'opacity'
+        colors: "background-color, border-color, color, fill, stroke",
+        opacity: "opacity",
       },
       opacity: {
-        '85': '0.85',
-        '80': '0.80',
+        85: "0.85",
+        80: "0.80",
       },
     },
     fontFamily: {
-      'ag-h1': ['Montserrat', 'sans-serif'],
-      'ag-h2': ['Montserrat', 'sans-serif'],
-      'ag-h3': ['Montserrat', 'sans-serif'],
-      'ag-h4': ['Montserrat', 'sans-serif'],
-      'ag-h5': ['Montserrat', 'sans-serif'],
-      'ag-h6': ['Montserrat', 'sans-serif'],
-      'ag-sub-heading': ['Montserrat', 'sans-serif'],
-      'ag-button-text': ['Montserrat', 'sans-serif'],
-      'ag-body-text': ['Montserrat', 'sans-serif'],
-      'ag-testimonial-text': ['Montserrat', 'sans-serif'],
-      'ag-list-text': ['Montserrat', 'sans-serif'],
-      'ag-404-text': ['Montserrat', 'sans-serif'],
+      "ag-h1": ["Montserrat", "sans-serif"],
+      "ag-h2": ["Montserrat", "sans-serif"],
+      "ag-h3": ["Montserrat", "sans-serif"],
+      "ag-h4": ["Montserrat", "sans-serif"],
+      "ag-h5": ["Montserrat", "sans-serif"],
+      "ag-h6": ["Montserrat", "sans-serif"],
+      "ag-sub-heading": ["Montserrat", "sans-serif"],
+      "ag-button-text": ["Montserrat", "sans-serif"],
+      "ag-body-text": ["Montserrat", "sans-serif"],
+      "ag-testimonial-text": ["Montserrat", "sans-serif"],
+      "ag-list-text": ["Montserrat", "sans-serif"],
+      "ag-404-text": ["Montserrat", "sans-serif"],
     },
     fontSize: {
-      'ag-h1': ['128px', { fontWeight: '700', lineHeight: 'auto' }], // Bold
-      'ag-h2': ['64px', { fontWeight: '700', lineHeight: '74px' }], // Bold
-      'ag-h3': ['48px', { fontWeight: '700', lineHeight: '58px' }], // Bold
-      'ag-h4': ['36px', { fontWeight: '700', lineHeight: '46px' }], // Bold
-      'ag-h5': ['24px', { fontWeight: '700', lineHeight: '34px' }], // Bold
-      'ag-h6': ['28px', { fontWeight: '700', lineHeight: '28px' }], // Bold
-      'ag-sub-heading': ['18px', { fontWeight: '500', lineHeight: 'auto' }], // Medium
-      'ag-button-text': ['16px', { fontWeight: '600', lineHeight: 'auto' }], // Semibold
-      'ag-body-text': ['16px', { fontWeight: '500', lineHeight: '26px' }], // Medium
-      'ag-testimonial-text': ['21px', { fontWeight: '600', fontStyle: 'italic', lineHeight: '31px' }], // Semibold Italic
-      'ag-list-text': ['16px', { fontWeight: '500', lineHeight: '46px' }], // Medium
-      'ag-404-text': ['269px', { fontWeight: '700', lineHeight: 'auto' }], // Bold
+      "ag-h1": ["128px", { fontWeight: "700", lineHeight: "auto" }], // Bold
+      "ag-h2": ["64px", { fontWeight: "700", lineHeight: "74px" }], // Bold
+      "ag-h3": ["48px", { fontWeight: "700", lineHeight: "58px" }], // Bold
+      "ag-h4": ["36px", { fontWeight: "700", lineHeight: "46px" }], // Bold
+      "ag-h5": ["24px", { fontWeight: "700", lineHeight: "34px" }], // Bold
+      "ag-h6": ["28px", { fontWeight: "700", lineHeight: "28px" }], // Bold
+      "ag-sub-heading": ["18px", { fontWeight: "500", lineHeight: "auto" }], // Medium
+      "ag-button-text": ["16px", { fontWeight: "600", lineHeight: "auto" }], // Semibold
+      "ag-body-text": ["16px", { fontWeight: "500", lineHeight: "26px" }], // Medium
+      "ag-testimonial-text": [
+        "21px",
+        { fontWeight: "600", fontStyle: "italic", lineHeight: "31px" },
+      ], // Semibold Italic
+      "ag-list-text": ["16px", { fontWeight: "500", lineHeight: "46px" }], // Medium
+      "ag-404-text": ["269px", { fontWeight: "700", lineHeight: "auto" }], // Bold
     },
   },
-    
-  
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('flowbite/plugin'),
-  ],
+
+  plugins: [require("@tailwindcss/typography"), require("flowbite/plugin")],
 };
-
-
